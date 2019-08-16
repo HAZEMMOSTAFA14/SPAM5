3f741fb78abb6eb302c969f1011510790b0202b9
HAZEMMOSTAFA14 <52508976+HAZEMMOSTAFA14@users.noreply.github.com>
2019-08-16 09:50:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("611827165176266752")
+let channel =     client.channels.get("611827649727168528")
 setInterval(function() {
 channel.send(`#daily credits`);
 }, 30)
